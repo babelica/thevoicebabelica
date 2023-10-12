6655bbed72bc5110512319e935637189bbf9e6db
Atlética Babélica - UnB <117299914+babelica@users.noreply.github.com>
2023-10-12 08:58:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,19 @@ const singersList = document.getElementById("singers-list");
 const skippedList = document.getElementById("skipped-list");
 const singerNameInput = document.getElementById("singer-name");
 
-function addSinger() {
-    const singerName = singerNameInput.value;
-    if (singerName) {
-        const listItem = document.createElement("li");
-        listItem.textContent = singerName;
-        singersList.appendChild(listItem);
-        singerNameInput.value = "";
+function skipSinger() {
+    const currentSinger = singersList.firstChild;
+    if (currentSinger) {
+        const singerName = currentSinger.textContent.trim();
+        singersList.removeChild(currentSinger);
         const skippedItem = document.createElement("li");
         skippedItem.textContent = singerName;
         skippedList.appendChild(skippedItem);
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "X";
-        removeButton.className = "remove-button";
-        removeButton.onclick = function() {
-            singersList.removeChild(listItem);
-            skippedList.removeChild(skippedItem);
-        };
-        listItem.appendChild(removeButton);
+    } else {
+        alert("Não há cantores na lista para pular.");
     }
+}
+
 }
 
 
