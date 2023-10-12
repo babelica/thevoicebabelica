f4c45a55467994776209c607521949fce17f2d29
Atlética Babélica - UnB <117299914+babelica@users.noreply.github.com>
2023-10-12 08:18:29 -0300

    botão de remover nome script.js

--- script.js
+++ script.js
@@ -3,21 +3,16 @@ const skippedList = document.getElementById("skipped-list");
 const singerNameInput = document.getElementById("singer-name");
 
 function addSinger() {
-    const singerName = singerNameInput.value;
-    if (singerName) {
-        const listItem = document.createElement("li");
-        listItem.textContent = singerName;
-        
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "X";
-        removeButton.onclick = function() {
-            singersList.removeChild(listItem);
-        };
-        listItem.appendChild(removeButton);
-        
-        singersList.appendChild(listItem);
-        singerNameInput.value = "";
-    }
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "X";
+    removeButton.className = "remove-button";
+    removeButton.onclick = function() {
+        singersList.removeChild(listItem);
+    };
+    listItem.appendChild(removeButton);
+}
+
 }
 
 function skipSinger() {
