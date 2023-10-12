41e742cb1cb879d76ffe42f441978c1851729091
Atlética Babélica - UnB <117299914+babelica@users.noreply.github.com>
2023-10-12 08:24:36 -0300

    Correção script.js

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const skippedList = document.getElementById("skipped-list");
 const singerNameInput = document.getElementById("singer-name");
 
 function addSinger() {
+    
+    }
+}
+
+function addSinger() {
+
+    const singerName = singerNameInput.value;
+    if (singerName) {
+        const listItem = document.createElement("li");
+        listItem.textContent = singerName;
+        singersList.appendChild(listItem);
+        singerNameInput.value = "";
 
     const removeButton = document.createElement("button");
     removeButton.textContent = "X";
